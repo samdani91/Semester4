e201f04db973b634cdf5fbbb65829caa713ad183
A. M Samdani Mozumder <114305338+samdani91@users.noreply.github.com>
2024-02-20 10:49:22 +0600

    Update task2.c

--- OS LAB/Lab-04/task2.c
+++ OS LAB/Lab-04/task2.c
@@ -1,74 +1,78 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define MAX_SIZE 100
-
-void multiplyRow(int row, int col, int mat1[][MAX_SIZE], int mat2[][MAX_SIZE], int result[][MAX_SIZE]) {
-    for (int j = 0; j < col; j++) {
-        int sum = 0;
-        for (int k = 0; k < col; k++) {
-            sum += mat1[row][k] * mat2[k][j];
-        }
-        result[row][j] = sum;
-    }
+void rowMultiply(int r1, int c1, int r2, int c2, int mat1[r1][c1], int mat2[r2][c2], int res[r1][c2], int i)
+{
+	for (int j = 0; j < c2; j++)
+	{
+		res[i][j] = 0;
+		for (int k = 0; k < c1; k++)
+		{
+			res[i][j] += mat1[i][k] * mat2[k][j];
+		}
+	}
 }
 
-int main() {
-    int row1, col1, row2, col2;
+int main()
+{
+	int r1, c1, r2, c2;
+	printf("Enter row and column for Matrix 1:");
+	scanf("%d %d", &r1, &c1);
+	printf("Enter row and column for Matrix 2:");
+	scanf("%d %d", &r2, &c2);
 
-    printf("Enter the number of rows and columns for matrix 1: ");
-    scanf("%d %d", &row1, &col1);
+	int mat1[r1][c1], mat2[r2][c2];
 
-    printf("Enter the number of rows and columns for matrix 2: ");
-    scanf("%d %d", &row2, &col2);
+	if (c1 != r2)
+	{
+		printf("Matrix Multiplication Not Possible\n");
+		return 0;
+	}
 
-    if (col1 != row2) {
-        printf("Matrices cannot be multiplied\n");
-        return 1;
-    }
+	int res[r1][c2];
 
-    int mat1[MAX_SIZE][MAX_SIZE], mat2[MAX_SIZE][MAX_SIZE], result[MAX_SIZE][MAX_SIZE];
+	printf("Enter matrix One\n");
+	for (int i = 0; i < r1; i++)
+	{
+		for (int j = 0; j < c1; j++)
+		{
+			scanf("%d", &mat1[i][j]);
+		}
+	}
 
-    printf("Enter elements of matrix 1:\n");
-    for (int i = 0; i < row1; i++) {
-        for (int j = 0; j < col1; j++) {
-            scanf("%d", &mat1[i][j]);
-        }
-    }
+	printf("Enter matrix Two\n");
+	for (int i = 0; i < r2; i++)
+	{
+		for (int j = 0; j < c2; j++)
+		{
+			scanf("%d", &mat2[i][j]);
+		}
+	}
 
-    printf("Enter elements of matrix 2:\n");
-    for (int i = 0; i < row2; i++) {
-        for (int j = 0; j < col2; j++) {
-            scanf("%d", &mat2[i][j]);
-        }
-    }
+	printf("Result Matrix:\n");
+	for (int i = 0; i < r1; i++)
+	{
+		int pid = fork();
 
-    pid_t pid;
-    for (int i = 0; i < row1; i++) {
-        pid = fork();
-        if (pid == 0) { // Child process
-            multiplyRow(i, col2, mat1, mat2, result);
-            printf("Row %d multiplication completed by process %d\n", i, getpid());
-            return 0;
-        } else if (pid < 0) { // Error
-            fprintf(stderr, "Fork failed\n");
-            return 1;
-        }
-    }
+		if (pid == -1)
+		{
+			perror("Fork");
+			exit(1);
+		}
+		else if (pid == 0)
+		{
+			rowMultiply(r1, c1, r2, c2, mat1, mat2, res, i);
+			for (int j = 0; j < c2; j++)
+			{
+				printf("%d ", res[i][j]);
+			}
+			printf("\n");
+			exit(1);
+		}
+		wait(NULL);
+	}
 
-    // Parent process waits for all child processes to complete
-    for (int i = 0; i < row1; i++) {
-        wait(NULL);
-    }
-
-    printf("Resultant matrix:\n");
-    for (int i = 0; i < row1; i++) {
-        for (int j = 0; j < col2; j++) {
-            printf("%d ", result[i][j]);
-        }
-        printf("\n");
-    }
-
-    return 0;
+	
 }
